daeb6a0f4545b4678d13e2498010b2996c4221ea
A1taxi-Hosur <a1taxihsr@gmail.com>
2025-10-17 18:47:44 +0530

    Updated app.config.js

--- app.config.js
+++ app.config.js
@@ -25,11 +25,7 @@ export default {
       supportsTablet: true
     },
     android: {
-      package: "com.a1taxi.driverpro",
-      adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
-        backgroundColor: "#ffffff"
-      }
+      package: "com.a1taxi.driverpro"
     },
     web: {
       bundler: "metro",
